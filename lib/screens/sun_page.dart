858299725ba77d983b92f166bb29b3663d35310b
Saidakbar23 <saidakbarjuraev@gmail.com>
2023-11-24 20:25:49 +0000

    Add body to the page

--- lib/screens/sun_page.dart
+++ lib/screens/sun_page.dart
@@ -5,16 +5,90 @@ class SunPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onVerticalDragDown: (DragDownDetails) {
-        Navigator.pop(context);
-      },
-      child: Container(
-        child: Text(
-          "This is sun page.",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
+    return Scaffold(
+      body: GestureDetector(
+        onVerticalDragDown: (DragDownDetails) {
+          Navigator.pop(context);
+        },
+        child: SafeArea(
+          child: Center(
+            child: Container(
+              child: Column(
+                children: [
+                  Image.asset(
+                    'images/sun.png',
+                    scale: 3.0,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Divider(
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'CENTER OF OUR SOLAR SYSTEM',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Divider(
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Text(
+                    'Sun',
+                    style: TextStyle(
+                      fontSize: 50.0,
+                      fontFamily: 'Angora',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Divider(
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '4.5 billion years old'.toUpperCase(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Divider(
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
