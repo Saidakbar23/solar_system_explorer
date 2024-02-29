ad61b5805b4592121bfb37916e045199ce510d09
Saidakbar23 <95351988+Saidakbar23@users.noreply.github.com>
2024-02-29 14:17:30 +0000

    working on jupiter page

--- lib/data/earth_data.dart
+++ lib/data/earth_data.dart
@@ -1,12 +1,20 @@
 const List<String> information = [
-  'Earth, our home planet, is a world unlike any other. The third planet from the sun, Earth is the only place in the known universe confirmed to host life.',
-  'With a radius of 3,959 miles, Earth is the fifth largest planet in our solar system, and it\'s the only one known for sure to have liquid water on its surface.',
-  'Earth orbits the sun once every 365.25 days. Since our calendar years have only 365 days, we add an extra leap day every four years to account for the difference. Though we can\'t feel it, Earth zooms through its orbit at an average velocity of 18.5 miles a second. During this circuit, our planet is an average of 93 million miles away from the sun, a distance that takes light about eight minutes to traverse.',
-  'At the planet\'s heart lies the inner core, a solid sphere of iron and nickel that\'s 759 miles wide and as hot as 9,800 degrees Fahrenheit. The inner core is surrounded by the outer core, a 1,400-mile-thick band of iron and nickel fluids. Beyond the outer core lies the mantle, a 1,800-mile-thick layer of viscous molten rock on which Earth\'s outermost layer, the crust, rests. On land, the continental crust is an average of 19 miles thick, but the oceanic crust that forms the seafloor is thinner—about three miles thick—and denser.',
-  'Earth\'s atmosphere is 78 percent nitrogen, 21 percent oxygen, and one percent other gases such as carbon dioxide, water vapor, and argon. Much like a greenhouse, this blanket of gases absorbs and retains heat. The atmosphere not only nourishes life on Earth, but it also protects it: It\'s thick enough that many meteorites burn up before impact from friction, and its gases—such as ozone—block DNA-damaging ultraviolet light from reaching the surface.'
+  'Earth – our home planet – is the third planet from the Sun, and the fifth largest planet. It\'s the only place we know of inhabited by living things.',
+  'While Earth is only the fifth largest planet in the solar system, it is the only world in our solar system with liquid water on the surface. Just slightly larger than nearby Venus, Earth is the biggest of the four planets closest to the Sun, all of which are made of rock and metal.',
+  'The name Earth is at least 1,000 years old. All of the planets, except for Earth, were named after Greek and Roman gods and goddesses. However, the name Earth is a Germanic word, which simply means “the ground.”',
+  'With an equatorial diameter of 7926 miles (12,760 kilometers), Earth is the biggest of the terrestrial planets and the fifth largest planet in our solar system. Earth is about 93 million miles away from the Sun. It takes about eight minutes for light from the Sun to reach our planet.',
+  'As Earth orbits the Sun, it completes one rotation every 23.9 hours. It takes 365.25 days to complete one trip around the Sun. That extra quarter of a day presents a challenge to our calendar system, which counts one year as 365 days. To keep our yearly calendars consistent with our orbit around the Sun, every four years we add one day. That day is called a leap day, and the year it\'s added to is called a leap year.',
+  'Earth is the only planet that has a single moon. Our Moon is the brightest and most familiar object in the night sky. In many ways, the Moon is responsible for making Earth such a great home. It stabilizes our planet\'s wobble, which has made the climate less variable over thousands of years.',
+  'When the solar system settled into its current layout about 4.5 billion years ago, Earth formed when gravity pulled swirling gas and dust in to become the third planet from the Sun. Like its fellow terrestrial planets, Earth has a central core, a rocky mantle, and a solid crust.',
+  'Earth is composed of four main layers, starting with an inner core at the planet\'s center, enveloped by the outer core, mantle, and crust.',
+  'Like Mars and Venus, Earth has volcanoes, mountains, and valleys. Earth\'s lithosphere, which includes the crust (both continental and oceanic) and the upper mantle, is divided into huge plates that are constantly moving. For example, the North American plate moves west over the Pacific Ocean basin, roughly at a rate equal to the growth of our fingernails. Earthquakes result when plates grind past one another, ride up over one another, collide to make mountains, or split and separate.',
+  'Near the surface, Earth has an atmosphere that consists of 78% nitrogen, 21% oxygen, and 1% other gases such as argon, carbon dioxide, and neon. The atmosphere affects Earth\'s long-term climate and short-term local weather and shields us from much of the harmful radiation coming from the Sun. It also protects us from meteoroids, most of which burn up in the atmosphere, seen as meteors in the night sky, before they can strike the surface as meteorites.',
 ];
 
-const List missions = ['150', '45', '482'];
+const List missions = [
+  '32',
+  '64',
+];
 const List images = [
   'images/earth_gallery/earth1.jpeg',
   'images/earth_gallery/earth2.jpeg',
