0d3f616947aa0026cea4bc148a42d69a33a02b64
Saidakbar23 <95351988+Saidakbar23@users.noreply.github.com>
2024-03-04 12:04:03 +0000

    Completed the Location Page for Each Planet

--- lib/screens/planets/mars/location.dart
+++ lib/screens/planets/mars/location.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class Location extends StatelessWidget {
+class Location extends StatefulWidget {
   const Location({super.key});
 
   @override
+  State<Location> createState() => _LocationState();
+}
+
+class _LocationState extends State<Location> {
+  var loadingPercentage = 0;
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (url) {
+          setState(() {
+            loadingPercentage = 0;
+          });
+        },
+        onProgress: (progress) {
+          setState(() {
+            loadingPercentage = 100;
+          });
+        },
+      ))
+      ..loadRequest(
+          Uri.parse('https://eyes.nasa.gov/apps/solar-system/#/mars'));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          '3 page',
-          style: TextStyle(
-            fontSize: 50.0,
+    return SafeArea(
+      child: Stack(
+        children: [
+          WebViewWidget(
+            controller: controller,
           ),
-        ),
+          if (loadingPercentage < 100)
+            LinearProgressIndicator(
+              value: loadingPercentage / 100.0,
+            )
+        ],
       ),
     );
   }
